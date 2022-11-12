d63e3db5b34605af9b3dbf7e5f6d43a36b6ef5a9
josar <josarroliga0943@outlook.es>
2022-11-11 22:19:25 -0600

    globales css y header

---
+++ src/components/header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header class="header">
+        <nav class="navbar flex">
+            <ul class="no-padding flex">
+                <li>Inicio</li>
+                <li>Sobre mi</li>
+                <li>Proyectos</li>
+            </ul>
+            <div class="button">
+                Boton
+            </div>
+        </nav>
+    </header>
+</template>
+
+
+<style scoped>
+@import '../assets/main.css';
+
+.header{
+    border-radius: 16px;
+    font-size: 2rem;
+}
+
+.navbar{
+    justify-content: space-between;
+    align-items: center;
+}
+
+ul{
+    gap: 4rem;
+}
+
+li{
+    list-style: none;
+}
+</style>
