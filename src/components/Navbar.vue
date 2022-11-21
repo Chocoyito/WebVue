c5cc5f66c0edf413bcbe26522b29c1c52261be0d
josar <josarroliga0943@outlook.es>
2022-11-20 20:23:41 -0600

    reestructurando

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<script setup>
+    
+</script>
+
+<template>
+    <nav class="navbar">
+        <a href="" class="navbar__logo">
+            R
+        </a>
+        <div>
+            <ul class="main-nav__list no-padding">
+                <li class="main-nav__item"><a href="">Inicio</a></li>
+                <li class="main-nav__item"><a href="">Sobre mi</a></li>
+                <li class="main-nav__item"><a href="">Proyectos</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+
+.navbar{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.main-nav__item{
+    list-style: none;
+}
+
+.navbar__logo{
+    padding: 1rem 2rem;
+    border: 2px solid var(--secundario);
+    border-radius: 16px;
+    font-weight: bold;
+}
+
+
+.main-nav__list{
+    gap: 4rem;
+    display: flex
+}
+</style>
