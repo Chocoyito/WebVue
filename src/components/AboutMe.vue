b9be630fdecfcae993488f38ee2ab9e30143dcec
josar <josarroliga0943@outlook.es>
2022-11-21 02:58:58 -0600

    vamos con proyectos supongo

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,7 +1,13 @@
 <script>
 
 export default {
-    name: "AboutMe"
+    name: "AboutMe",
+    data(){
+        return{
+            nombre_completo: "Roberto Jose Amador Arroliga",
+            fecha_de_nacimiento: "09/04/2003"
+        }
+    }
 }
 
 </script>
@@ -11,25 +17,32 @@ export default {
 
 
         <div class="about-me">
-            <h1 class="no-margin">
-                Mas sobre mi
-            </h1>
-            <h2 class="no-margin">
-                Proximamente 💀
-            </h2>
+            <div class="about-me__info">
+                <article class="about-me__yo">
+                    <h2>Quien soy</h2>
+                    <picture class="about-me__picture">
+                        <img class="about-me__img" src="../assets/icons/yo.jpg" alt="yo">
+                    </picture>
+                </article>
+
+                <article>
+                    <h2>Educacion</h2>
+                    <p>Once you’ve accepted your flaws, no one can use them against you.</p>
+                </article>
+                <article>
+                    <h2>Carrera</h2>
+                    <p>Once you’ve accepted your flaws, no one can use them against you.</p>
+                </article>
+            </div>
         </div>
 
-<!--        <picture class="about-me__picture">
-            <img class="about-me__img" src="../assets/icons/yo.jpg">
-        </picture>
-
-        <div class="about-me__info">
-            <h2 class="no-margin">Quien soy</h2>
-            <p>Adolescens duis proin orci dolore suscipit quis dicant tale. Constituam duo appetere constituam dolore.
-                Elaboraret et dicant velit aliquet. Quem nec laoreet maximus ludus invenire curabitur lectus.</p>-->
+        <!--        <div class="about-me__info">
+                    <h2 class="no-margin">Quien soy</h2>
+                    <p>Adolescens duis proin orci dolore suscipit quis dicant tale. Constituam duo appetere constituam dolore.
+                        Elaboraret et dicant velit aliquet. Quem nec laoreet maximus ludus invenire curabitur lectus.</p>-->
 
 
-        </div>
+    </div>
 </template>
 
 
@@ -38,34 +51,44 @@ export default {
     padding: 0 2rem;
     width: auto;
     margin: 5rem 0 0 0;
-    color: #376DF6;
 }
-.about-me {
-    display: flex;
-    text-align: center;
-    font-size: 25px;
-    flex-direction: column;
-}
+
+
 @media (min-width: 1080px) {
-    .container{
+    .about-me{
+        height: 600px;
+    }
+    .container {
         padding: 0 15rem;
         margin: 10rem 0 0 0;
+    }
+
+    .about-me__info{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+    .about-me__yo{
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        width: 300px;
+        color: #376DF6;
+        border: 2px solid #376DF6;
+        border-radius: 32px;
+        height: 600px;
     }
 }
 
 
 
-/*.about-me__picture {
-    width: 350px;
-    height: 350px;
-    display: flex;
-    justify-content: center;
-}
-
 .about-me__img {
     border-radius: 50%;
+    width: 150px;
+    height: 150px;
 }
 
+/*
 .about-me__info {
     text-align: center;
     padding: 0 50px;
@@ -73,7 +96,8 @@ export default {
 
 .about-me__info h2 {
     font-size: 50px;
-}*/
+}
+*/
 
 
 </style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,29 @@
 <template>
+    <div class="container">
+        <div :style="{textAlign:'center'}">
+            <h2>Mis proyectos</h2>
+            <div class="my-projects">
+                <article>
+                    <h3>Tal cosa</h3>
+                    <picture>
 
+                    </picture>
+                </article>
+                <article>
+                    <h3>Tal cosa</h3>
+                    <picture>
+
+                    </picture>
+                </article>
+                <article>
+                    <h3>Tal cosa</h3>
+                    <picture>
+
+                    </picture>
+                </article>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +33,14 @@ export default {
 </script>
 
 <style scoped>
-
+@media (min-width: 1080px) {
+    .container {
+        padding: 0 15rem;
+        margin: 10rem 0 0 0;
+    }
+    .my-projects{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+    }
+}
 </style>
